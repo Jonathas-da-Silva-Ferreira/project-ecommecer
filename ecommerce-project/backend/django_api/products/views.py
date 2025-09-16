9d4408a56ea3b8e0fced0a67ed83ced0fdeed120
Jonathas da Silva Ferreira <105074747+Jonathas-da-Silva-Ferreira@users.noreply.github.com>
2025-09-16 11:33:35 -0300

    news updates

--- ecommerce-project/backend/django_api/products/views.py
+++ ecommerce-project/backend/django_api/products/views.py
@@ -23,4 +23,32 @@ def create_product(request):
 @api_view(['GET'])
 def retrieve_product(request, pk):
     """Retrieve a product by its ID"""
+    try:
+         product = Product.objects.get(pk=pk)
+         serializer = ProductSerializer(product)
+         return Response(serializer.data)
+    except Product.DoesNotExist:
+        return Response({"Erro": "Produto não encontrado." }, status=status.HTTP_404_NOT_FOUND)
+
+@api_view(['PUT'])
+def update_product(request, pk):
+    """Update a product by its ID"""
+    try:
+        product = Product.objects.get(pk=pk)
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    except Product.DoesNotExist:
+        return Response({"Erro": "Produto não encontrado."}, status=status.HTTP_404_NOT_FOUND)
     
+@api_view(['DELETE'])
+def delete_product(request, pk):
+    """Delete a product by its ID"""
+    try:
+        product =  Product.objects.get(pk=pk)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    except Product.DoesNotExist:
+        return Response({"Erro": "Produto não encontrado."}, status=status.HTTP_404_NOT_FOUND)
